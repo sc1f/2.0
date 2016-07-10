1d7ce58f9ce98ea61b5ac07602867d1a28a0e360
sc1f <tanjunyuan@hotmail.co.uk>
2016-07-10 17:34:11 -0400

    more changes. markup for category + posts.

---
+++ js/main.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var helpers = {
+    debounce: function debounce(func, wait, immediate) {
+        var timeout;
+        return function() {
+            var context = this,
+                args = arguments;
+            var later = function() {
+                timeout = null;
+                if (!immediate) func.apply(context, args);
+            };
+            var callNow = immediate && !timeout;
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+            if (callNow) func.apply(context, args);
+        };
+    }
+};
+
+var jumbotron = {
+    init : function(e, i) {
+        if (e) {
+            var delay = 0;
+            $(i).each(function(){
+                $(this).delay(delay).addClass('fadeIn');
+                delay+=50;
+            });
+            console.log(delay);
+        } else {
+            console.log('ya sure fucked up.');
+        }
+    }
+};
+
+$(document).ready(function() {
+    $('.nav').each(function(i) {
+        $(this).delay((i++) * 50).fadeTo(500, 1);
+    });
+    jumbotron.init('.jumbotron-container', '.jumbotron-item');
+});
+
+/* TODO: cron jobs for compression of images, gzipping files for server, serverside maintenance */
