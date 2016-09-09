3beb735f48027b64b7e406ba3f11b5c7bab80220
sc1f <tanjunyuan@hotmail.co.uk>
2016-09-09 17:16:37 -0500

    more front end

--- js/main.js
+++ js/main.js
@@ -18,14 +18,28 @@ var helpers = {
     }
 };
 
+var homepageControl = {
+    init: function(control, element) {
+        $(element).hide();
+        $(control).click(function() {
+            if($(element).css('display') === 'none') {
+                $(element).slideDown();
+            } else if ($(element).css('display') !== 'none') {
+                $(element).slideUp();
+            }
+        });
+    }
+}
+
 
 $(document).ready(function() {
     console.log('Welcome to my portfolio site!\n\nIf you\'re reading this, you\'re probably interested in the technical details:\n \n* This site was built with Jekyll.');
     $("#jumbotron__item--biography--dynamic").typed({
         strings: ["design.", "code.", "communicate.", "photograph.", "build."],
-        typeSpeed: 10,
+        typeSpeed: 5,
         loop: true
     });
+    homepageControl.init('.homepage__item--action.design', '.homepage__item--list.design');
 });
 
 /* TODO: cron jobs for compression of images, gzipping files for server, serverside maintenance */
