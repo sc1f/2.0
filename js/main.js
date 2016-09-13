c7bc594c1133e5f95b5127ba0749b81282348276
sc1f <tanjunyuan@hotmail.co.uk>
2016-09-13 12:46:39 -0500

    begin posts + img

--- js/main.js
+++ js/main.js
@@ -3,10 +3,10 @@
 var helpers = {
     debounce: function debounce(func, wait, immediate) {
         var timeout;
-        return function() {
+        return function () {
             var context = this,
                 args = arguments;
-            var later = function() {
+            var later = function () {
                 timeout = null;
                 if (!immediate) func.apply(context, args);
             };
@@ -15,31 +15,136 @@ var helpers = {
             timeout = setTimeout(later, wait);
             if (callNow) func.apply(context, args);
         };
-    }
+    },
+    visibility: (function ($) {
+
+        /**
+         * Copyright 2012, Digital Fusion
+         * Licensed under the MIT license.
+         * http://teamdf.com/jquery-plugins/license/
+         *
+         * @author Sam Sehnert
+         * @desc A small plugin that checks whether elements are within
+         *       the user visible viewport of a web browser.
+         *       only accounts for vertical position, not horizontal.
+         */
+        var $w = $(window);
+        $.fn.visible = function (partial, hidden, direction) {
+
+            if (this.length < 1)
+                return;
+
+            var $t = this.length > 1 ? this.eq(0) : this,
+                t = $t.get(0),
+                vpWidth = $w.width(),
+                vpHeight = $w.height(),
+                direction = (direction) ? direction : 'both',
+                clientSize = hidden === true ? t.offsetWidth * t.offsetHeight : true;
+
+            if (typeof t.getBoundingClientRect === 'function') {
+
+                // Use this native browser method, if available.
+                var rec = t.getBoundingClientRect(),
+                    tViz = rec.top >= 0 && rec.top < vpHeight,
+                    bViz = rec.bottom > 0 && rec.bottom <= vpHeight,
+                    lViz = rec.left >= 0 && rec.left < vpWidth,
+                    rViz = rec.right > 0 && rec.right <= vpWidth,
+                    vVisible = partial ? tViz || bViz : tViz && bViz,
+                    hVisible = partial ? lViz || rViz : lViz && rViz;
+
+                if (direction === 'both')
+                    return clientSize && vVisible && hVisible;
+                else if (direction === 'vertical')
+                    return clientSize && vVisible;
+                else if (direction === 'horizontal')
+                    return clientSize && hVisible;
+            } else {
+
+                var viewTop = $w.scrollTop(),
+                    viewBottom = viewTop + vpHeight,
+                    viewLeft = $w.scrollLeft(),
+                    viewRight = viewLeft + vpWidth,
+                    offset = $t.offset(),
+                    _top = offset.top,
+                    _bottom = _top + $t.height(),
+                    _left = offset.left,
+                    _right = _left + $t.width(),
+                    compareTop = partial === true ? _bottom : _top,
+                    compareBottom = partial === true ? _top : _bottom,
+                    compareLeft = partial === true ? _right : _left,
+                    compareRight = partial === true ? _left : _right;
+
+                if (direction === 'both')
+                    return !!clientSize && ((compareBottom <= viewBottom) && (compareTop >= viewTop)) && ((compareRight <= viewRight) && (compareLeft >= viewLeft));
+                else if (direction === 'vertical')
+                    return !!clientSize && ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+                else if (direction === 'horizontal')
+                    return !!clientSize && ((compareRight <= viewRight) && (compareLeft >= viewLeft));
+            }
+        };
+
+    })(jQuery)
 };
 
 var homepageControl = {
     init: function(control, element) {
         $(element).hide();
         $(control).click(function() {
-            if($(element).css('display') === 'none') {
-                $(element).slideDown();
-            } else if ($(element).css('display') !== 'none') {
-                $(element).slideUp();
+            var txt = $(element).is(':visible') ? 'See More' : 'See Less';
+            $(control).text(txt);
+            $(element).slideToggle();
+        });
+    },
+    toTop: function(jumbotron, trigger){
+        if(jumbotron){
+            if($(jumbotron).visible(true)) {
+                $(trigger).css('display', 'none');
             }
+            $(document).scroll(function(){
+                if ($(jumbotron).visible(true)) {
+                    $(trigger).css('display', 'none');
+                } else {
+                    $(trigger).css('display', 'initial');
+                }
+            });
+        } else {
+            $(document).scroll(function(){
+                if ($(window).scrollTop() == 0) {
+                    $(trigger).css('display', 'none')
+                } else {
+                    $(trigger).css('display', 'initial');
+                }
+            })
+        }
+
+        $(trigger).click(function() {
+            $("html, body").animate({ scrollTop: 0 }, 400);
+            return false;
         });
     }
 }
 
 
 $(document).ready(function() {
+    //initialize lazy-loader
+    new Blazy({
+        selector: '.lazy, img'
+    });
+
+    // init wow
+    new WOW().init({
+        mobile: false
+    });
     console.log('Welcome to my portfolio site!\n\nIf you\'re reading this, you\'re probably interested in the technical details:\n \n* This site was built with Jekyll.');
     $("#jumbotron__item--biography--dynamic").typed({
-        strings: ["design.", "code.", "communicate.", "photograph.", "build."],
+        strings: ["design.", "code.", "communicate.", "photograph.", "build.", "create."],
         typeSpeed: 5,
         loop: true
     });
     homepageControl.init('.homepage__item--action.design', '.homepage__item--list.design');
+    homepageControl.init('.homepage__item--action.code', '.homepage__item--list.code');
+    homepageControl.init('.homepage__item--action.photo', '.homepage__item--list.photo');
+    homepageControl.toTop('.jumbotron', '#to-top');
 });
 
 /* TODO: cron jobs for compression of images, gzipping files for server, serverside maintenance */
